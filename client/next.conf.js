458073a8f24b3d808dd578578f18b5025b58dec8
IvanSheichuk <124913211+IvanSheichuk@users.noreply.github.com>
2023-02-10 15:27:42 +0200

    Update next.conf.js

--- client/next.conf.js
+++ client/next.conf.js
@@ -5,5 +5,6 @@ module.exports = {
     env: {
         REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL,
         REACT_APP_HOME_PIC_URL = process.env.REACT_APP_HOME_PIC_URL,
+        REACT_APP_BASE_PIC_URL = process.env.REACT_APP_BASE_PIC_URL,
     },
 }
